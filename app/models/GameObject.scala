ccf749eef63d373ec74de3488a45b886036da095
Atsushi Iwasa <a-iwasa@biglobe.co.jp>
2014-05-30 22:30:40 +0900

    6人の場合、パーシヴァルをなしにした

--- app/models/GameObject.scala
+++ app/models/GameObject.scala
@@ -28,7 +28,7 @@ class GameObject(members:List[String]) {
   def isAssassin(username: String):Boolean = { username == Assassin }
   def isEvil(username: String):Boolean = { Evils.contains(username) }
   def getJustices():List[String] = { players diff Evils }
-  def existPercival:Boolean = { players.length > 5 }
+  def existPercival:Boolean = { players.length > 6 }
   def existLady:Boolean = { players.length > 6 }
   def getQuestMembersCount:Int = { questMembersCount(players.length)(questCount) }
 }
